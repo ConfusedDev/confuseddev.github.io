6318a9c22a300e4a173739e6b661010fed81576b
ConfusedDev <54333889+ConfusedDev@users.noreply.github.com>
2022-03-16 12:48:43 -0400

    add scrollspy move code sample to new button

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function newsletter(){
   window.location.href = "https://quiet-shore-88177.herokuapp.com/";
 }
+
+function codeSample(){
+  window.location.href = "https://github.com/ConfusedDev/code-sample";
+}
